6b6bf53491b389245524b025e325dd5d5173042e
mwallschlaeger <marcel.wallschlaeger@zalf.de>
2024-06-26 14:04:27 +0200

    merged scripts

--- create_datasets.py
+++ create_datasets.py
@@ -3,15 +3,15 @@ import geodatasets as gds
 import os
 
 datasets_list = list(gds.data.flatten().keys())
-print(len(datasets_list))
 
 size_datasets = []
 for u in range(len(datasets_list)):
     dataset = gpd.read_file(gds.get_path(datasets_list[u]))
+    if not isinstance(dataset, gpd.geodataframe.GeoDataFrame):
+      continue
     dataset.to_file("./datasets/" + str(u) + ".geojson", driver="GeoJSON")
     # Size in MegaBytes
     size_datasets.append(
         os.stat("./datasets/" + str(u) + ".geojson").st_size / (1024 * 1024)
     )
 
-gpd.hist(size_datasets)
