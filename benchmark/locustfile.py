aa34871af07c393e3fe3e0ae2ea73ecbf70b25e6
silvadealmeida <igotode@gmail.com>
2024-06-27 10:18:52 +0200

    .

--- benchmark/locustfile.py
+++ benchmark/locustfile.py
@@ -1,6 +1,7 @@
 from locust import task, run_single_user
 from locust import FastHttpUser
 import os
+import glob
 
 
 class GeonodeLoadTest(FastHttpUser):
@@ -9,18 +10,30 @@ class GeonodeLoadTest(FastHttpUser):
     host = "https://stable.demo.geonode.org"
     login = os.environ["GEONODE_LOGIN"]
     password = os.environ["GEONODE_PASSWORD"]
+
+    dataset_list = glob.glob("./datasets/*.geojson")
     open_dataset_nr = 1825
-    dataset_title = "Dia_phan_Tinh"
+    dataset_title_download = "Dia_phan_Tinh"
+    dataset_title_upload = "test_upload"
     type_file = "application/json"
-    file = "./benchmark/datasets.1.geojson"
-    datasets = (
-        "geojson_file",
+    file = "./datasets/1.geojson"
+    datasets = [
         (
-            "test.geojson",
-            open("./datasets/0.geojson", "rb"),
-            "application/json",
+            "geojson_file",
+            (
+                "test_upload",
+                open("./datasets/0.geojson", "rb"),
+                "application/json",
+            ),
         ),
-    )
+    ]
+    dataset_title = "dataset_title"
+    dataset_nr_to_upload_metadata = 1
+    metada_title = "metada_example.xml"
+    metada_file = "./datasets/metada_example.xml"
+    metadata = (metada_title, open(metada_file, "rb"), "text/xml")
+
+    dataset_nr_to_delete = 1
 
     @task
     def login(self):
@@ -45,12 +58,12 @@ class GeonodeLoadTest(FastHttpUser):
         ) as resp:
             pass
         with self.client.get(  # 3.Check the metadata
-            f"/datasets/geonode_master_data:geonode:{self.dataset_title}/metadata_detail",
+            f"/datasets/geonode_master_data:geonode:{self.dataset_title_download}/metadata_detail",
             catch_response=True,
         ) as resp:
             pass
         with self.client.get(  # 4.Dataset Download
-            f"/datasets/geonode:{self.dataset_title}/dataset_download",
+            f"/datasets/geonode:{self.dataset_title_download}/dataset_download",
             catch_response=True,
         ) as resp:
             pass
@@ -59,26 +72,38 @@ class GeonodeLoadTest(FastHttpUser):
             catch_response=True,
         ) as resp:
             pass
+        # Sometimes can't work in the stablegeonode because of flood
         with self.client.post(  # 5.UPLOAD DATASET: Upload dataset with API
             "/api/v2/uploads/upload",
             headers={
                 "Authorization": "Basic dXNlcjpwYXNzd29yZA==",
             },
-            files=[self.datasets],
+            files=[
+                (
+                    "geojson_file",
+                    (
+                        "test_upload2",
+                        open("./datasets/0.geojson", "rb"),
+                        "application/json",
+                    ),
+                )
+            ],
             catch_response=True,
         ) as resp:
             pass
-        with self.client.get(  # TODO Edit Advance metadata
-            "/",
-            catch_response=True,
-        ) as resp:
-            pass
-        with self.client.delete(  # TODO Delete dataset
-            "/api/v2/resources/1",
-            headers={"Authorization": "Basic dXNlcjpwYXNzd29yZA=="},
-            catch_response=True,
-        ) as resp:
-            pass
+        # with self.client.put(  # TODO Upload Metada with API
+        #     f"/api/v2/datasets/{self.dataset_nr_to_upload_metadata}/metadata",
+        #     headers={"Authorization": "Basic dXNlcjpwYXNzd29yZA=="},
+        #     files=[self.metadata],
+        #     catch_response=True,
+        # ) as resp:
+        #     pass
+        # with self.client.delete(  # TODO Delete dataset
+        #     f"/api/v2/resources/{self.dataset_nr_to_delete}/delete",
+        #     headers={"Authorization": "Basic dXNlcjpwYXNzd29yZA=="},
+        #     catch_response=True,
+        # ) as resp:
+        #     pass
         with self.client.post(  # logout
             "/account/logout/",
             headers={
