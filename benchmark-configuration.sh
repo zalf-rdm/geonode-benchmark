21a8a20a2bbf473e7272756db8e843d906c69753
Marcel Wallschlaeger <mwarcel.wallschlaeger@zalf.de>
2024-09-23 14:32:28 +0200

    WIP

---
+++ benchmark-configuration.sh
@@ -0,0 +1,7 @@
+export GN_BENCHMARK_CLUSTER_DOMAIN="draven.cluster"
+export GN_BENCHMARK_INGRESS_CLASS="nginx-external"
+export GN_BENCHMARK_EXTERNAL_DOMAIN=https://geonode-benchmark.draven.cluster.zalf.de
+export GN_BENCHMARK_STORAGE_CLASS="nfs-csi-retain"
+
+export GEONODE_API_BASIC_AUTH=YWRtaW46Z2Vvbm9kZQ==
+export GEONODE_API_URL="https://geonode-benchmark.draven.cluster.zalf.de/api/v2/"
