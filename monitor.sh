43af6db4c05caba41d3296352b52dc6f2f720f0f
Marcel Wallschlaeger <mwarcel.wallschlaeger@zalf.de>
2024-09-25 21:42:04 +0200

    WIP

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# requires kubectl
+pods_str=$(kubectl get pods -o json | jq -r ".items[].metadata.name")
+pods_arr=($pods_str)
+echo ${pods_arr[@]} 
+EPOCH=$(date +%s)
+for pod in ${pods_arr[@]}; do
 
-#while 1=1; do
-kubectl top pods --containers --no-headers=false
+    kubectl get --raw /apis/metrics.k8s.io/v1beta1/namespaces/geonode/pods/$pod |  jq  -r  --arg EPOCH "$EPOCH" '.containers[] | [ .name, .usage.cpu, .usage.memory, $EPOCH] | @csv'
+    #echo "pod: " $pod
+done
